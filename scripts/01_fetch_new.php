e20276333fac5016d60ada64c9e59be1faf3999b
auto commit <noreply@localhost>
2022-07-23 17:08:25 +0800

    auto update @ 2022-07-23 17:07:39

---
+++ scripts/01_fetch_new.php
@@ -0,0 +1,131 @@
+<?php
+require 'vendor/autoload.php';
+
+$basePath = dirname(__DIR__);
+
+$projectYears = [date('Y') - 1911];
+$cities = ['基隆市', '臺北市', '新北市', '桃園市', '新竹縣', '新竹市', '苗栗縣', '臺中市', '南投縣', '彰化縣', '雲林縣', '嘉義縣', '嘉義市', '臺南市', '高雄市', '屏東縣', '宜蘭縣', '花蓮縣', '臺東縣', '金門縣', '澎湖縣', '連江縣'];
+
+use Goutte\Client;
+
+$client = new Client();
+
+$crawler = $client->request('GET', 'https://landchg.tcd.gov.tw/Module/RWD/Web/pub_exhibit.aspx');
+$form = $crawler->filter('form')->form();
+
+$domDocument = new \DOMDocument;
+$ff = $domDocument->createElement('input');
+$ff->setAttribute('name', '__EVENTTARGET');
+$ff->setAttribute('value', 'ctl00$page_content$ChgPointMarker');
+$formInput = new \Symfony\Component\DomCrawler\Field\InputFormField($ff);
+$form->set($formInput);
+
+$ff = $domDocument->createElement('input');
+$ff->setAttribute('name', '__EVENTARGUMENT');
+$ff->setAttribute('value', '');
+$formInput = new \Symfony\Component\DomCrawler\Field\InputFormField($ff);
+$form->set($formInput);
+
+$ff = $domDocument->createElement('input');
+$ff->setAttribute('name', '__SCROLLPOSITIONX');
+$ff->setAttribute('value', '0');
+$formInput = new \Symfony\Component\DomCrawler\Field\InputFormField($ff);
+$form->set($formInput);
+
+$ff = $domDocument->createElement('input');
+$ff->setAttribute('name', '__SCROLLPOSITIONY');
+$ff->setAttribute('value', '433');
+$formInput = new \Symfony\Component\DomCrawler\Field\InputFormField($ff);
+$form->set($formInput);
+
+$ff = $domDocument->createElement('input');
+$ff->setAttribute('name', 'h_lat');
+$ff->setAttribute('value', '23.042379299657025');
+$formInput = new \Symfony\Component\DomCrawler\Field\InputFormField($ff);
+$form->set($formInput);
+
+$ff = $domDocument->createElement('input');
+$ff->setAttribute('name', 'h_lng');
+$ff->setAttribute('value', '120.40802721756668');
+$formInput = new \Symfony\Component\DomCrawler\Field\InputFormField($ff);
+$form->set($formInput);
+
+$ff = $domDocument->createElement('input');
+$ff->setAttribute('name', 'h_zoom');
+$ff->setAttribute('value', '11');
+$formInput = new \Symfony\Component\DomCrawler\Field\InputFormField($ff);
+$form->set($formInput);
+
+$ff = $domDocument->createElement('input');
+$ff->setAttribute('name', 'ProjectYear');
+$ff->setAttribute('value', '111');
+$formInput = new \Symfony\Component\DomCrawler\Field\InputFormField($ff);
+$form->set($formInput);
+
+$ff = $domDocument->createElement('input');
+$ff->setAttribute('name', 'City');
+$ff->setAttribute('value', '桃園市');
+$formInput = new \Symfony\Component\DomCrawler\Field\InputFormField($ff);
+$form->set($formInput);
+
+$ff = $domDocument->createElement('input');
+$ff->setAttribute('name', 'selectMap');
+$ff->setAttribute('value', '3');
+$formInput = new \Symfony\Component\DomCrawler\Field\InputFormField($ff);
+$form->set($formInput);
+
+$ff = $domDocument->createElement('input');
+$ff->setAttribute('name', 'PublicImg');
+$ff->setAttribute('value', '-1');
+$formInput = new \Symfony\Component\DomCrawler\Field\InputFormField($ff);
+$form->set($formInput);
+
+foreach ($projectYears as $projectYear) {
+    $rawPath = $basePath . '/raw/' . $projectYear;
+    if (!file_exists($rawPath)) {
+        mkdir($rawPath, 0777, true);
+    }
+    $dataPath = $basePath . '/data/csv/' . $projectYear;
+    if (!file_exists($dataPath)) {
+        mkdir($dataPath, 0777, true);
+    }
+    foreach ($cities as $city) {
+        $targetFile = $rawPath . '/' . $city . '.html';
+        $crawler = $client->submit($form, ['City' => $city, 'ProjectYear' => $projectYear]);
+        file_put_contents($targetFile, $client->getResponse()->getContent());
+        $fh = fopen($dataPath . '/' . $city . '.csv', 'w');
+        $headerDone = false;
+
+        $content = file_get_contents($targetFile);
+
+        $pos = strpos($content, 'function markerBind');
+        if (false !== $pos) {
+            $posEnd = strpos($content, '</script>', $pos);
+            $part = substr($content, $pos, $posEnd - $pos);
+            $lines = explode(');', $part);
+            foreach ($lines as $line) {
+                $line = str_replace('\'', '', $line);
+                $parts = explode(',', $line);
+                if (count($parts) === 4) {
+                    $dataLine = [];
+                    $parts2 = explode('<br/>', $parts[2]);
+                    foreach ($parts2 as $part) {
+                        $parts3 = explode('：', $part);
+                        $dataLine[$parts3[0]] = $parts3[1];
+                    }
+                    if (!isset($dataLine['變異類型'])) {
+                        $dataLine['變異類型'] = '';
+                    }
+
+                    $dataLine['latitude'] = trim(substr($parts[0], strrpos($parts[0], '(') + 1));
+                    $dataLine['longitude'] = trim($parts[1]);
+                    if (false === $headerDone) {
+                        $headerDone = true;
+                        fputcsv($fh, array_keys($dataLine));
+                    }
+                    fputcsv($fh, $dataLine);
+                }
+            }
+        }
+    }
+}
